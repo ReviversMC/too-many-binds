45e110b6420deae531c262fa70d1fe3cc6095ad1
Julian Burner <burnerjulian@gmail.com>
2022-06-29 16:44:36 +0200

    Remove unused import

--- toomanybinds-1.18.2/src/main/java/com/github/reviversmc/toomanybinds/TooManyBinds1182.java
+++ toomanybinds-1.18.2/src/main/java/com/github/reviversmc/toomanybinds/TooManyBinds1182.java
@@ -3,8 +3,6 @@ package com.github.reviversmc.toomanybinds;
 import com.github.reviversmc.toomanybinds.autocompletion.VanillaKeybindSuggestions;
 import com.github.reviversmc.toomanybinds.autocompletion.VanillaKeybindSuggestions1182;
 
-import net.minecraft.client.MinecraftClient;
-
 public class TooManyBinds1182 extends TooManyBinds117 {
 
 	@Override
